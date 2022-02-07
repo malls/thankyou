8eba9db2cd88a971956b722b902c4ffd32fc326f
Forrest Almasi <_@forrestalmasi.com>
2022-02-07 14:40:16 -0500

    start backend work

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
+
+const options = {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['html', 'css', 'js', 'ico'],
+  index: false,
+  maxAge: '1d',
+  redirect: false,
+  setHeaders: function (res, path, stat) {
+    res.set('x-timestamp', Date.now())
+  }
+};
+
+app.use(express.static('public', options));
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
+
